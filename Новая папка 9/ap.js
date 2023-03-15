49499b706828704728c99745aa297596880ac8ab
Saparoviskender <127577144+Saparoviskender@users.noreply.github.com>
2023-03-15 11:42:40 +0600

    Add files via upload

---
+++ Новая папка 9/ap.js
@@ -0,0 +1,40 @@
+// 1)
+// const N1 = Number(prompt())
+// const N2 = Number(prompt())
+
+// function hello (N1, N2) {
+//       return alert('минимальное'+ Math.min(N1, N2))
+// }
+
+// hello(N1,N2)
+
+
+// 2)
+// const dln = prompt()
+// function Dlny (dln) {
+//    console.log(dln.length);
+// }
+// Dlny (dln)
+
+// 3)
+// function calc() {
+   
+//        const menu =  prompt ( "Калькулятор: 1) сложение, 2) вычитание, 3)умножение, 4) деление");
+   
+//        const a = Number(prompt("a")) 
+
+//        const b = Number(prompt("b")) 
+
+//        const result = [false, a + b, a - b, a * b, a / b][menu];
+
+//        return result === false || result === Infinity || result === undefined ? 'Wrong operation' : result;
+   
+//    };
+
+//    alert(calc());
+   
+
+
+
+
+
